d8d6b1235fd813b2888e774ffedada83b86df649
bss <baekss0409@gmail.com>
2020-09-01 21:31:57 +0900

    jpashop 회원 등록 api

---
+++ src/main/java/jpabook/jpashop/api/MemberApiController.java
@@ -0,0 +1,51 @@
+package jpabook.jpashop.api;
+
+import javax.validation.Valid;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import jpabook.jpashop.domain.Member;
+import jpabook.jpashop.service.MemberService;
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequiredArgsConstructor
+public class MemberApiController {
+
+	private final MemberService memberService;
+	
+	@PostMapping("/api/v1/members")
+	public CreateMemberResponse saveMemberV1(@RequestBody @Valid Member member) {
+		Long id = memberService.join(member);
+		return new CreateMemberResponse(id);
+	}
+	
+	//api 스펙을 한눈에 알아볼 수 있고, Entity 속성 변화에도 유연하게 대처 가능하도록 요청과 응답 모두 dto를 쓴다.
+	@PostMapping("/api/v2/members")
+	public CreateMemberResponse saveMemberV2(@RequestBody @Valid CreateMemberRequest request) {
+		
+		Member member = new Member();
+		member.setName(request.getName());
+		
+		Long id = memberService.join(member);
+		return new CreateMemberResponse(id);
+	}
+	
+	@Data
+	static class CreateMemberRequest {
+		private String name;
+	}
+	
+	@Data
+	static class CreateMemberResponse {
+		private Long id;
+
+		public CreateMemberResponse(Long id) {
+			this.id = id;
+		}
+	}
+	
+}
