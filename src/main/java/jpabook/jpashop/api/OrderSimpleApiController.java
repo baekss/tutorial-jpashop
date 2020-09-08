0519d23adf3393321870912c57f99129101a5674
bss <baekss0409@gmail.com>
2020-09-08 23:29:38 +0900

    jpashop 주문 조회 api Ver.3

--- src/main/java/jpabook/jpashop/api/OrderSimpleApiController.java
+++ src/main/java/jpabook/jpashop/api/OrderSimpleApiController.java
@@ -54,6 +54,15 @@ public class OrderSimpleApiController {
 		return orders;
 	}
 	
+	@GetMapping("/api/v3/simple-orders")
+	public List<SimpleOrderDto> ordersV3(){
+		List<Order> orders = orderRepository.findAllWithMemberDelivery();
+		List<SimpleOrderDto> result = orders.stream()
+				.map(o->new SimpleOrderDto(o))
+				.collect(Collectors.toList());
+		return result;
+	}
+	
 	@Data
 	static class SimpleOrderDto {
 		private Long orderId;
