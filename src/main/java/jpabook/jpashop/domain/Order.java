04393723cc23b101816f86aa6a3eabcc5c4a0b21
bss <baekss0409@gmail.com>
2020-09-12 14:21:56 +0900

    jpashop 주문 상세 조회 api Ver.1

--- src/main/java/jpabook/jpashop/domain/Order.java
+++ src/main/java/jpabook/jpashop/domain/Order.java
@@ -35,8 +35,6 @@ public class Order {
 	@Column(name="order_id")
 	private Long id;
 	
-	private String name;
-	
 	@ManyToOne(fetch=FetchType.LAZY)
 	@JoinColumn(name="member_id")
 	private Member member;
