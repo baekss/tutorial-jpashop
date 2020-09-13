6f14410a3a0ea1a9fe464f790b02583c8bc3c7d9
bss <baekss0409@gmail.com>
2020-09-13 15:20:40 +0900

    jpashop 주문 상세 조회 api Ver.3

--- src/main/java/jpabook/jpashop/repository/OrderRepository.java
+++ src/main/java/jpabook/jpashop/repository/OrderRepository.java
@@ -104,4 +104,22 @@ public class OrderRepository {
 				" join fetch o.delivery d", Order.class
 				).getResultList();
 	}
+	
+	public List<Order> findAllWithItem() {
+		//fetch join 시 1:N 관계에 의해 Order 1개에 OrderItem이 N개 이므로 결과도 카디널리티 곱으로 나온다.
+		//따라서 동일한 Order를 N개로 받지 않기 위해 JPQL의 distinct를 적용한다.
+		//JPQL의 distinct는 SQL상으로도 distinct를 붙여주며, 반환된 Entity가 중복일 땐 중복을 걸러서 반환해 주는 역할을 한다. 
+		return em.createQuery(
+				"select distinct o from Order o"+
+				" join fetch o.member m"+
+				" join fetch o.delivery d"+
+				" join fetch o.orderItems oi"+
+				" join fetch oi.item i", Order.class)
+				//.setFirstResult(1)
+				//.setMaxResults(10)
+				.getResultList();
+		//collection fetch join 단점은 페이징 처리시 SQL상으로 페이징을 적용(ex. limit, offset 키워드)하지 않는다는 점이다.
+		//경고를 띄우며 어플리케이션 메모리 상에서 페이징 처리를 한다(firstResult/maxResults specified with collection fetch; applying in memory!).
+		//따라서 메모리 관련 Error가 발생할 수도 있다.
+	}
 }
